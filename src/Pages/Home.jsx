4cbf52b7558ff88263f62d3686c1aa20f5999267
AhmedMeribout <102850406+AhmedMeribout@users.noreply.github.com>
2024-10-14 14:32:50 +0200

    removed duplicate tryhackme badge in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,11 +12,12 @@ const Home = () => {
 
       <div className="flex flex-col justify-center w-1/2 space-y-4 pl-10">
         <div>
-        <img src="https://tryhackme-badges.s3.amazonaws.com/ahmedmeribout02.png" alt="TryHackMe" className="w-1/2 mb-8"></img>
           <img  src="https://tryhackme-badges.s3.amazonaws.com/ahmedmeribout02.png" alt="TryHackMe" className="w-1/2 mb-8" ></img>
+          <div>
           <div className="text-white text-4xl">Hi There! 👋</div>
           <div className="text-white text-4xl">
             I'M <span className="text-green-400">MERIBOUT AHMED YAHIA</span>
+          </div>
           </div>
         </div>
         <div className="text-green-300 text-2xl">
